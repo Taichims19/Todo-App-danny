24a9aa593a14215ecf1f8ffd6b9e301f41afd3a7
Daniel Mendez <danielms694@gmail.com>
2024-08-06 00:04:18 -0400

    ajustes visuales en login y register

--- src/auth/pages/RegisterPage.tsx
+++ src/auth/pages/RegisterPage.tsx
@@ -6,6 +6,7 @@ import {
   TextField,
   Alert,
   Typography,
+  Box,
 } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { useAuthContext } from "../../context/AuthContext";
@@ -61,79 +62,171 @@ export const RegisterPage = () => {
   };
 
   return (
-    <AuthLayout title="Crear Cuenta">
-      <form
-        onSubmit={handleSubmit}
-        className="animate__animated animate__fadeIn animate__faster"
+    <AuthLayout title="">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          backgroundColor: "#f5f5f5",
+          padding: "20px",
+          background: "rgb(50, 130, 181)",
+          borderRadius: 3,
+        }}
       >
-        <Grid container>
-          <Grid item xs={12} md={6} sx={{ mt: 2 }}>
-            <TextField
-              label="Nombre completo"
-              type="text"
-              placeholder="Tu Nombre"
-              fullWidth
-              name="displayName"
-              value={displayName}
-              onChange={onInputChange}
-              error={!!displayNameValid && formSubmitted}
-              helperText={displayNameValid}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} sx={{ mt: 2 }}>
-            <TextField
-              label="Correo"
-              type="email"
-              placeholder="correo@gmail.com"
-              fullWidth
-              name="email"
-              value={email}
-              onChange={onInputChange}
-              error={!!emailValid && formSubmitted}
-              helperText={emailValid}
-            />
-          </Grid>
-          <Grid item xs={12} md={6} sx={{ mt: 2 }}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              placeholder="contraseña"
-              fullWidth
-              name="password"
-              value={password}
-              onChange={onInputChange}
-              error={!!passwordValid && formSubmitted}
-              helperText={passwordValid}
-            />
-          </Grid>
-          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-            <Grid
-              item
-              xs={12}
-              display={
-                !!displayNameValid || !!emailValid || !!passwordValid
-                  ? ""
-                  : "none"
-              }
-            >
-              <Alert severity="error">
-                Por favor complete el formulario correctamente
-              </Alert>
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: "400px",
+            minHeight: "80vh",
+            padding: "20px",
+            backgroundColor: "white",
+            boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+            borderRadius: "8px",
+          }}
+        >
+          <Typography variant="h4" component="h1" gutterBottom align="center">
+            Registro
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  label="Nombre completo"
+                  type="text"
+                  placeholder="Tu Nombre"
+                  fullWidth
+                  name="displayName"
+                  value={displayName}
+                  onChange={onInputChange}
+                  error={!!displayNameValid && formSubmitted}
+                  helperText={displayNameValid}
+                  sx={{
+                    "& .MuiInputLabel-root": {
+                      color: "rgba(122, 49, 240, 0.8)", // Color del label cuando está en estado normal
+                    },
+                    "& .MuiInputLabel-shrink": {
+                      color: "rgba(122, 49, 240, 0.8) !important", // Color del label cuando está arriba
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.5)", // Color del borde
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.7)", // Color del borde cuando pasa el mouse
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.9)", // Color del borde cuando está enfocado
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Correo"
+                  type="email"
+                  placeholder="correo@gmail.com"
+                  fullWidth
+                  name="email"
+                  value={email}
+                  onChange={onInputChange}
+                  error={!!emailValid && formSubmitted}
+                  helperText={emailValid}
+                  sx={{
+                    "& .MuiInputLabel-root": {
+                      color: "rgba(122, 49, 240, 0.8)", // Color del label cuando está en estado normal
+                    },
+                    "& .MuiInputLabel-shrink": {
+                      color: "rgba(122, 49, 240, 0.8) !important", // Color del label cuando está arriba
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.5)", // Color del borde
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.7)", // Color del borde cuando pasa el mouse
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.9)", // Color del borde cuando está enfocado
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Contraseña"
+                  type="password"
+                  placeholder="contraseña"
+                  fullWidth
+                  name="password"
+                  value={password}
+                  onChange={onInputChange}
+                  error={!!passwordValid && formSubmitted}
+                  helperText={passwordValid}
+                  sx={{
+                    "& .MuiInputLabel-root": {
+                      color: "rgba(122, 49, 240, 0.8)", // Color del label cuando está en estado normal
+                    },
+                    "& .MuiInputLabel-shrink": {
+                      color: "rgba(122, 49, 240, 0.8) !important", // Color del label cuando está arriba
+                    },
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.5)", // Color del borde
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.7)", // Color del borde cuando pasa el mouse
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "rgba(122, 49, 240, 0.9)", // Color del borde cuando está enfocado
+                      },
+                    },
+                  }}
+                />
+              </Grid>
+
+              <Grid item xs={12} sx={{ marginTop: 2 }}>
+                <Alert severity="error">
+                  Por favor complete el formulario correctamente
+                </Alert>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                  sx={{
+                    background: "rgba(122, 49, 240, 0.8)",
+                    height: "38px",
+                    "&:hover": {
+                      background: "rgba(122, 49, 240, 0.6)",
+                    },
+                  }}
+                >
+                  Crear Cuenta
+                </Button>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                sx={{ textAlign: "center", marginTop: "10px" }}
+              >
+                <Typography>
+                  ¿Ya tienes una cuenta?{" "}
+                  <Link component={RouterLink} color="inherit" to="/auth/login">
+                    Iniciar sesión
+                  </Link>
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Button type="submit" variant="contained" fullWidth>
-                Crear Cuenta
-              </Button>
-            </Grid>
-          </Grid>
-          <Grid container direction="row" justifyContent="end">
-            <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
-            <Link component={RouterLink} color="inherit" to="/auth/login">
-              Iniciar sesión
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
+          </form>
+        </Box>
+      </Box>
     </AuthLayout>
   );
 };
